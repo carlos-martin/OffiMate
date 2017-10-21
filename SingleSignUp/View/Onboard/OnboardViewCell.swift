446cf7c709b5c0cd17e19bb15c45aac791e995f6
Carlos Martin <carlos.martin@tuva.co>
2017-10-21 18:40:56 +0200

    office: New OnBoard view design

---
+++ SingleSignUp/View/Onboard/OnboardViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardViewCell.swift
+//  SingleSignUp
+//
+//  Created by Carlos Martin on 20/10/2017.
+//  Copyright © 2017 Carlos Martin. All rights reserved.
+//
+
+import UIKit
+
+class OnboardSignUpViewCell: UITableViewCell {
+    @IBOutlet weak var signupButton: UIButton!
+}
+
+class OnboardLoginViewCell: UITableViewCell {
+    @IBOutlet weak var loginButton: UIButton!
+}
